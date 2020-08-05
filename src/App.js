389153e29f700eba568c34d39fe42a586be8d5b7
philipjpark <66290921+philipjpark@users.noreply.github.com>
2020-08-05 01:15:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+      });
+    }, 1000);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if(nextState.count % 3 === 0) return true;
+    else return false;
+  }
+
+  reset = () => {
+    this.setState({
+      count: this.state.count = 0,
+    });
+
+  };
+
+  render() {
+    return (
+      <div>
+        <p>The ticker number is : {this.state.count} </p>
+        <button type="button" onClick={this.reset}>Reset Button</button>
+      </div>
+    );
+  }
+}
+
+export default Ticker;
